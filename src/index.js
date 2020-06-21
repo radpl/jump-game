2f87341bef9e03275c7b5015af46d953f6b1fc17
Radoslaw Plonka <radoslaw.a.plonka@gmail.com>
2020-06-21 17:30:26 +0200

    Furter improvements

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { Application as PIXIApplication } from 'pixi.js';
+import { Application as PIXIApplication, Graphics } from 'pixi.js';
 import { setupKeyboardEvents, hitTest, detectBoundaries, randomIntFromInterval, detectOutside } from './utils';
-import { drawCircle, drawLine, drawRect } from './shapes';
+import { drawCircle, drawLine, drawRect, drawReward } from './shapes';
 
 const app = new PIXIApplication({ width: 800, height: 412 });
 document.body.appendChild(app.view);
@@ -11,18 +11,45 @@ app.stage.addChild(line);
 
 const arrlength = 10;
 const rects = [];
+const rewards = [];
+
 for (let i = 0; i < arrlength; i++) {
   let positionY = linePosition;
   if (i % 2 === 0)
-    positionY = randomIntFromInterval(250, 280);
-  const positionX = randomIntFromInterval(1064, 2000);
+    positionY = linePosition - 150;
+  const positionX = randomIntFromInterval(app.renderer.view.width, app.renderer.view.width + 50) + i * 150;
   const rectangle = drawRect(positionX, positionY);
   app.stage.addChild(rectangle);
   rects.push(rectangle);
+
+  //draw rewards
+  for (let j = 5; j < 150; j += 15) {
+    const rew = drawReward(rectangle.x, rectangle.y - 17, j);
+    rectangle.rewards.push(rew);
+    rewards.push(rew);
+    app.stage.addChild(rew);
+  }
+
 }
 
-const circle = drawCircle(linePosition);
+
+
+
+const circle = drawCircle(linePosition, 0.45);
 app.stage.addChild(circle);
+
+// const circle2 = new Graphics();
+// circle2.beginFill(0xAAAAAA);
+// circle2.lineStyle(1, 26112, 1);
+// //circle2.arc(0, 0, 15, arcAngle, 2 * Math.PI - arcAngle, false);
+// //circle2.arc(0, 0, 30, 0.25, 2 * Math.PI - 0.25, false);
+// circle2.lineTo(0, 0);
+// circle2.arc(0, 0, 30, Math.PI + 0.25, Math.PI + 2 * Math.PI - 0.25, false);
+// circle2.lineTo(0, 0);
+// circle2.endFill();
+// circle2.x = 100;
+// circle2.y = 200;
+// app.stage.addChild(circle2);
 
 // Listen for frame updates
 app.ticker.add(() => {
@@ -31,6 +58,11 @@ app.ticker.add(() => {
 
   const col = rects.some((rectangle) => hitTest(circle, rectangle));
   circle.hitDetected(col);
+
+  rewards.forEach(reward => {
+    const col = hitTest(circle, reward);
+    if (col) reward.renderReward = false;
+  })
 
   boundaries = detectBoundaries(circle, { x: 5, y: 5, width: app.renderer.view.width, height: linePosition + 15, });
   circle.boundariesDetected(boundaries);
--- src/shapes.js
+++ src/shapes.js
@@ -4,18 +4,26 @@
 /* eslint-disable no-undef */
 import { Graphics as PIXIGraphics } from 'pixi.js';
 
-export const drawCircle = (positionY) => {
+export const drawCircle = (positionY, arcAngle) => {
   const newCircle = new PIXIGraphics();
 
   newCircle.beginFill(0xAAAAAA);
   newCircle.lineStyle(1, 26112, 1);
-  newCircle.drawCircle(0, 0, 15);
+  newCircle.x = 100;
+  newCircle.y = positionY;
+  //newCircle.drawCircle(0, 0, 15);
+  newCircle.arc(0, 0, 15, arcAngle, 2 * Math.PI - arcAngle, false);
+  //circle.arc(0, 0, 30, 0.25, 2 * Math.PI - 0.25, false);
+  newCircle.lineTo(0, 0);
   newCircle.endFill();
   newCircle.x = 100;
-  //newCircle.y = positionY;
+  newCircle.y = positionY;
   newCircle.vx = 0;
   newCircle.vy = 0;
   newCircle.radius = 15;
+  newCircle.arcAngle = arcAngle;
+  newCircle.arcAngleSpeed = 0.05;
+  newCircle.myDirection = false;
 
   newCircle.accelerationX = 0;
   newCircle.accelerationY = 0;
@@ -33,6 +41,8 @@ export const drawCircle = (positionY) => {
     this.vy += 0.1;
     this.x += this.vx;
     this.y += this.vy;
+    if (this.arcAngle > 0.45 || this.arcAngle < 0) this.arcAngleSpeed = -this.arcAngleSpeed;
+    this.arcAngle += this.arcAngleSpeed;
   }
 
   newCircle.hitDetected = function (check) {
@@ -42,8 +52,16 @@ export const drawCircle = (positionY) => {
     } else {
       this.beginFill(0xAAAAAA);
     }
+    console.log(this.arcAngle);
     this.lineStyle(1, 26112, 1);
-    this.drawCircle(0, 0, 15);
+    if (this.myDirection) {
+      this.arc(0, 0, 15, Math.PI + this.arcAngle, Math.PI + 2 * Math.PI - this.arcAngle, false);
+    } else {
+      this.arc(0, 0, 15, this.arcAngle, 2 * Math.PI - this.arcAngle, false);
+
+    }
+
+    this.lineTo(0, 0);
     this.endFill();
   };
 
@@ -94,10 +112,12 @@ export const drawCircle = (positionY) => {
       case 'left':
         this.accelerationX = -this.speed;
         this.frictionX = 1;
+        this.myDirection = true;
         break;
       case 'right':
         this.accelerationX = this.speed;
         this.frictionX = 1;
+        this.myDirection = false;
         break;
       case 'up':
         this.accelerationY = -this.speed;
@@ -122,15 +142,50 @@ export const drawLine = (width, positionY) => {
   return newLine;
 };
 
+export const drawReward = (x, y, pos) => {
+  const newReward = new PIXIGraphics();
+  //newReward.clear();
+  newReward.beginFill(0xAAAAAA);
+  newReward.lineStyle(1, 26112, 1);
+  newReward.drawCircle(0, 0, 5);
+  newReward.endFill();
+  newReward.x = x + pos;
+  newReward.pos = pos;
+  newReward.y = y;
+  newReward.renderReward = true;
+
+  newReward.redraw = function (x, y) {
+    if (this.renderReward) {
+      this.clear();
+      this.beginFill(0xAAAAAA);
+      this.lineStyle(1, 26112, 1);
+      this.drawCircle(0, 0, 5);
+      this.endFill();
+      this.x = x + this.pos;
+      this.y = y;
+    } else {
+      this.clear();
+    }
+
+  }
+
+  return newReward;
+
+}
+
 export const drawRect = (orgX, positionY) => {
   const newRect = new PIXIGraphics();
-
+  newRect.rewards = [];
   newRect.redraw = function () {
     this.lineStyle(1, 0xFFFFFF, 1);
-    this.drawRect(0, 0, 56, 46);
+    this.drawRect(0, 0, 150, 46);
     this.alpha = 0.5;
     this.y = positionY - 46;
     this.orgX = orgX;
+
+    for (let i = 0; i < this.rewards.length; i++) {
+      this.rewards[i].redraw(this.x, this.y - 17);
+    }
   };
 
   newRect.shouldRedraw = function () { return this.x + this.width < 0; };
@@ -139,6 +194,9 @@ export const drawRect = (orgX, positionY) => {
     if (this.shouldRedraw()) {
       this.clear();
       this.x = this.orgX;
+      for (let i = 0; i < this.rewards.length; i++) {
+        this.rewards[i].renderReward = true;
+      }
     } else {
       this.clear();
       this.redraw();
